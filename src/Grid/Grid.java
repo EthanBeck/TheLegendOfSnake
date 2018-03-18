4aa1ff5552e413fe4763390024269db3d1843822
EthanBeck <ethbeck2@gmail.com>
2018-03-18 13:06:39 -0500

    Change width
    Change width of grid form 1500 to 2000

--- src/Grid/Grid.java
+++ src/Grid/Grid.java
@@ -1,7 +1,7 @@
 package Grid;
 
 public class Grid {
-	public static final int width = 1500;
+	public static final int width = 2000;
 	public static final int height = 1500;
 	public static final int border = 10;
 }
